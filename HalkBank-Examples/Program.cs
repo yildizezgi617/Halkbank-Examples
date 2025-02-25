361f31679939c756c13da431663650d146715475
Ezgi Yıldız <yildizezgi617@gmail.com>
2025-02-25 09:44:49 +0300

    Add project files.

---
+++ HalkBank-Examples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HalkBank_Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
